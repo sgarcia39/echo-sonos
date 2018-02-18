177c07be823bc6006e1013d51c72095b9221e59b
Steven Garcia <s.garcia39@gmail.com>
2018-02-18 13:25:27 -0800

    Fix : Inject utterances array into intents object

--- echo/build.js
+++ echo/build.js
@@ -3,7 +3,8 @@
 const exec = require("child_process").exec;
 const fs = require("fs");
 const path = require("path");
-const JSON5 = require("JSON5");
+// const JSON5 = require("JSON5");
+const JSON5 = JSON;
 
 const processArguments = process.argv.slice(2);
 const directoryPath = __dirname;
@@ -47,11 +48,10 @@ const buildCore = function() {
     exportObject[interactionModelKey][invocationNameKey] = invocationNameValue;
 };
 
-// TODO: Build value with synonyms feature
+// TODO: Implement custom synonyms
 const buildTypes = function() {
-    // INFO: Copy import types array
-    const exportTypesArray = JSON.parse(JSON.stringify(importObject[typesKey]));
-    exportObject[interactionModelKey][languageModelKey][typesKey] = exportTypesArray;
+    const typesArray = JSON.parse(JSON.stringify(importObject[typesKey]));
+    exportObject[interactionModelKey][languageModelKey][typesKey] = typesArray;
 };
 
 const buildIntents = function() {
@@ -61,8 +61,8 @@ const buildIntents = function() {
         const sampleLine = samplesArray[samplesArrayIdx].trim();
         const sampleSeperatorIdx = sampleLine.indexOf(" ");
         if (sampleLine.length > 0 && sampleSeperatorIdx > -1) {
-            let sampleKey = sampleLine.slice(0, sampleSeperatorIdx);
-            let sampleValue = sampleLine.slice(sampleSeperatorIdx + 1);
+            const sampleKey = sampleLine.slice(0, sampleSeperatorIdx);
+            const sampleValue = sampleLine.slice(sampleSeperatorIdx + 1);
             if (!samplesObject[sampleKey]) {
                 samplesObject[sampleKey] = [];
             }
@@ -70,20 +70,14 @@ const buildIntents = function() {
         }
     }
 
-    // INFO : Copy import intents array
-    const importIntentsArray = JSON.parse(JSON.stringify(importObject[intentsKey]));
-    const exportIntentsArray = [];
-    console.log(JSON.stringify(importObject));
-    for (let importIntentsArrayIdx = 0; importIntentsArrayIdx < importIntentsArray.length; ++importIntentsArrayIdx) {
-        const importIntentObject = importIntentsArray[importIntentsArrayIdx];
-        // console.log(samplesObject[importIntentObject[intentNameKey]]);
-        if (importIntentObject[intentNameKey] && samplesObject[importIntentObject[intentNameKey]]) {
-            // console.log("foobar?");
-            importIntentObject[samplesKey] = samplesObject[importIntentObject[intentNameKey]];
-            exportIntentsArray.push(importIntentObject[samplesKey]);
-        }
+    const intentsArray = JSON.parse(JSON.stringify(importObject[intentsKey]));
+    for (let intentsArrayIdx = 0; intentsArrayIdx < intentsArray.length; ++intentsArrayIdx) {
+        const intentObject = intentsArray[intentsArrayIdx];
+        const intentSamplesArray = samplesObject[intentObject[intentNameKey]];
+        intentObject[samplesKey] = intentSamplesArray ? intentSamplesArray : [];
     }
-    exportObject[interactionModelKey][languageModelKey][intentsKey] = exportIntentsArray;
+
+    exportObject[interactionModelKey][languageModelKey][intentsKey] = intentsArray;
 };
 
 const start = function() {
@@ -95,7 +89,7 @@ const start = function() {
     buildTypes();
     buildIntents();
 
-    // console.log(JSON.stringify(exportObject));
+    console.log("export: " + JSON.stringify(exportObject));
 };
 
 start();
